ac0059a3fa7b80014ab4aece8739c065ff35e56a
illutek <stefanvandenborne@gmail.com>
2018-09-27 10:55:36 +0200

    start script from footer to .js file

---
+++ js/email_spam.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Stefan on 17/11/2017.
+ * Strict mode makes it easier to write "secure" JavaScript.
+ *
+  * (function () {
+ *   })(); = IIFE
+ */
+
+(function () {
+    'use strict';
+
+})();
