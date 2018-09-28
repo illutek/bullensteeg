b69d22eb0ffdf3859f65f2e344dd24322c1a18f2
illutek <stefanvandenborne@gmail.com>
2018-09-28 09:29:10 +0200

    contact info in footer and header

--- js/email_spam.js
+++ js/email_spam.js
@@ -1,12 +1,20 @@
 /**
- * Created by Stefan on 17/11/2017.
- * Strict mode makes it easier to write "secure" JavaScript.
- *
-  * (function () {
- *   })(); = IIFE
- */
+ * Created by Stefan on 17/09/2018.
+ * 
+*/
 
-(function () {
-    'use strict';
-
-})();
+{ coded = "Xjg1JJhSj11Jh0@0Xj41.wQX"
+    key = "qDcpzYtJCRdyvIFimSGV30oNHBwa75KnLlurZfO4X8W2UeQ6TE9jsAkhb1MgxP"
+    shift=coded.length
+    link=""
+    for (i=0; i<coded.length; i++) {
+      if (key.indexOf(coded.charAt(i))==-1) {
+        ltr = coded.charAt(i)
+        link += (ltr)
+        } else {    
+          ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+          link += (key.charAt(ltr))
+        }
+    }
+      document.write("<a class='btn btn--green u-align-center u-full-width' href='mailto:"+link+"'>Contacteer mij via e-mail.</a>")
+}
